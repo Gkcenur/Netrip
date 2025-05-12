155d2ffbe51c46c071d8308ef02f82d78981b3cc
Gokcenur <kocakgokcenur@gmail.com>
2025-05-12 16:20:03 +0300

    login and signup screens are completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
--- app/src/main/java/com/example/netrip/HomeActivity.kt
+++ app/src/main/java/com/example/netrip/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
